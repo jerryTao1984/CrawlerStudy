3df4e95e7c1fe0ffae5af4602261dab57955dfb2
陶俊win <77045842@2qq.com>
2020-12-15 21:33:51 +0800

    xpath

--- common/FileUtil.py
+++ common/FileUtil.py
@@ -1,7 +1,15 @@
 import json
+
+import requests
+
+
 class FileUtil(object):
+
     def __init__(self):
         super().__init__()
+        self.__header__ = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+        }
 
     #保存字符串到文件
     def saveTxtToFile(self, fileName, result):
@@ -13,7 +21,12 @@ class FileUtil(object):
         with open(fileName, 'a', encoding='utf-8') as fp:
             json.dump(jsonResult, fp=fp, ensure_ascii=False)
 
-
+    #写入byte到文件
     def saveBToFile(self, fileName, result):
         with open(fileName, 'wb') as fp:
             fp.write(result)
+    #下载网页写入文件
+    def downLoadToFile(self,url,fileName):
+        bimgdata = requests.get(url=url, headers=self.__header__).content
+        with open(fileName, 'wb') as fp:
+            fp.write(bimgdata)
